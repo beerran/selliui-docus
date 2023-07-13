32ca4db53b883e571e0780cfc48fce66596deda9
Emil Larsson <larsson.emil@hotmail.com>
2023-07-13 21:04:45 +0200

    .

--- app.config.ts
+++ app.config.ts
@@ -1,23 +1,17 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'SelliUi',
+    description: 'SelliUi',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
+      twitter: 'selliondk'
     },
     github: {
       dir: '.starters/default/content',
       branch: 'main',
       repo: 'docus',
       owner: 'nuxt-themes',
-      edit: true
+      edit: false
     },
     aside: {
       level: 0,
